60d5b46293e2344e757279a9b63236f13bf836d2
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-03-01 14:48:43 -0500

    Update casa.js

--- casa.js
+++ casa.js
@@ -1,4 +1,7 @@
-
+function techo(){
+console.log("  _______/\\__")
+console.log("/______________\\")
+}
 function piso(){
 console.log("[    _   _    ]")
 console.log("[   [_] [_]   ]")
@@ -9,10 +12,7 @@ console.log("[      _      ]")
 console.log("[     / \\     ]")
 console.log("[____/[_]\\____]")
 }
-function techo(){
-console.log("  _______/\\__")
-console.log("/______________\\")
-}
+
 function casa(nPisos){
     techo()
     for(let i = 0; i < nPisos; i++){
